a2e66aa877b8160c9d8315d26f52fa9b4c3b6ff2
Rahul Choudhary <rahulssss.mail@gmail.com>
2022-08-09 16:02:51 -0400

    first commit

---
+++ javascript/first.js
@@ -0,0 +1,35 @@
+let a="Rahul";
+let b=6;  // let b="6 is sting"
+
+/*console.log(a+b);
+
+console.log(typeof a);
+
+const c={
+name:"Rahul",
+section:1,
+isPrincipal:false,
+};
+//c="25";
+c['friends']="dinesh";
+
+console.log(c);
+
+const words={
+    Speed:"running fast",
+    Hello:"Running slow"
+}
+console.log(words)*/
+
+let c=1233554555.4545545664;
+let d;
+let e=null;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e); // showing data type object
+
+
+
