0ca172407e3532fca72d9e0083646dc05da95583
Rahul Choudhary <rahulssss.mail@gmail.com>
2022-08-11 16:56:09 -0400

    Second commit

---
+++ javascript/conditional.js
@@ -0,0 +1,64 @@
+/*const i=959;*/
+
+/*  if(i % 7 == 0){
+    console.log("Divisible by seven");
+}
+else{
+    console.log("Not Divisible by seven");
+}*/
+
+/*  if(i%3==0){
+    console.log("Multiple of three");
+}
+else{
+    console.log("Not a multiple of three")
+}
+*/
+
+
+/*  if(i>=0){
+    console.log("Positive Number");
+}
+else{
+    console.log("Not Positive");
+}
+*/
+
+/*
+if(i%10==4){
+    console.log("Digit has 4 in its Unit Place");
+    }
+    else{
+        console.log("Digit don't have 4 in its Unit Place");
+    }
+    */
+
+    /*
+    if(i>99&&i<1000){
+        console.log("Number has 3 Digits");
+        }
+        else{
+            console.log("Number has no 3 digits");
+        }
+        */
+
+        /*
+        {
+            const a= i>99&&i<999?"Three digit number":"Not three digit number";
+        console.log(a);
+        }
+        */
+
+        const a=45;
+        const b=256;
+        const c=4;
+        if(d=a>b&&a>c){
+            d=a;
+        }
+        else if(b>a&&b>c){
+            d=b;
+        }
+        else{
+            d=c;
+        }
+     console.log(d+"Greatest");
